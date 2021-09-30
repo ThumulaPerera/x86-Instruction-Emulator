6d54c64f575e212cb6d1c218d8af5fba86018376
Budvin Chathura <chathura97rc@gmail.com>
2021-10-01 02:26:07 +0530

    Add addressing with SIB byte

--- emulator/ModRM.hpp
+++ emulator/ModRM.hpp
@@ -4,6 +4,8 @@
 #include <stdexcept>
 #include <cstring>
 #include "Utils.hpp"
+#include "Storage.hpp"
+#include "SIB.hpp"
 
 class ModRM
 {
@@ -41,53 +43,80 @@ public:
         {
         case 0b00:
         {
-            if (rm == 0b100 || rm == 0b101)
-            {
-                throw std::logic_error("rm=100 and rm=101 mode not implemented for mod=00");
-            }
 
             output.storage_type = MEMORY;
 
-            struct StorageArgs intermediate_storage_args;
-            intermediate_storage_args.storage_type = R32;
-            intermediate_storage_args.address = rm;
-            output.address = this->storage->load<int32_t>(intermediate_storage_args);
+            if (rm == 0b100)
+            {
+                output.address = SIB::getScaledIndex(this->sequence, this->sequence_current_index, mod, this->storage);
+            }
+            else if (rm == 0b101)
+            {
+                int32_t displacement;
+                std::memcpy(&displacement, &(this->sequence[*this->sequence_current_index]), sizeof(int32_t));
+                (*this->sequence_current_index) += sizeof(int32_t);
+                output.address = displacement;
+            }
+            else
+            {
+                struct StorageArgs intermediate_storage_args;
+                intermediate_storage_args.storage_type = R32;
+                intermediate_storage_args.address = rm;
+                output.address = this->storage->load<int32_t>(intermediate_storage_args);
+            }
+
             break;
         }
 
         case 0b01:
         {
-            if (rm == 0b100)
-            {
-                throw std::logic_error("rm=100 mode not implemented for mod=01");
-            }
-            int32_t displacement = (int32_t)(this->sequence[(*this->sequence_current_index)++]);
 
             output.storage_type = MEMORY;
 
-            struct StorageArgs intermediate_storage_args;
-            intermediate_storage_args.storage_type = R32;
-            intermediate_storage_args.address = rm;
-            output.address = this->storage->load<int32_t>(intermediate_storage_args) + displacement;
+            if (rm == 0b100)
+            {
+                int32_t SIB_value = SIB::getScaledIndex(this->sequence, this->sequence_current_index, mod, this->storage);
+                int32_t displacement = (int32_t)(this->sequence[(*this->sequence_current_index)++]);
+                output.address = SIB_value + displacement;
+            }
+            else
+            {
+                int32_t displacement = (int32_t)(this->sequence[(*this->sequence_current_index)++]);
+
+                struct StorageArgs intermediate_storage_args;
+                intermediate_storage_args.storage_type = R32;
+                intermediate_storage_args.address = rm;
+                output.address = this->storage->load<int32_t>(intermediate_storage_args) + displacement;
+            }
+
             break;
         }
 
         case 0b10:
         {
-            if (rm == 0b100)
-            {
-                throw std::logic_error("rm=100 mode not implemented for mod=10");
-            }
-            int32_t displacement;
-            std::memcpy(&displacement, &(this->sequence[*this->sequence_current_index]), sizeof(int32_t));
-            (*this->sequence_current_index) += sizeof(int32_t);
 
             output.storage_type = MEMORY;
 
-            struct StorageArgs intermediate_storage_args;
-            intermediate_storage_args.storage_type = R32;
-            intermediate_storage_args.address = rm;
-            output.address = this->storage->load<int32_t>(intermediate_storage_args) + displacement;
+            if (rm == 0b100)
+            {
+                int32_t SIB_value = SIB::getScaledIndex(this->sequence, this->sequence_current_index, mod, this->storage);
+
+                int32_t displacement;
+                std::memcpy(&displacement, &(this->sequence[*this->sequence_current_index]), sizeof(int32_t));
+                (*this->sequence_current_index) += sizeof(int32_t);
+                output.address = SIB_value + displacement;
+            }
+            else
+            {
+                int32_t displacement;
+                std::memcpy(&displacement, &(this->sequence[*this->sequence_current_index]), sizeof(int32_t));
+                (*this->sequence_current_index) += sizeof(int32_t);
+                struct StorageArgs intermediate_storage_args;
+                intermediate_storage_args.storage_type = R32;
+                intermediate_storage_args.address = rm;
+                output.address = this->storage->load<int32_t>(intermediate_storage_args) + displacement;
+            }
+
             break;
         }
 
---
+++ emulator/SIB.hpp
@@ -0,0 +1,57 @@
+#ifndef SIB_H
+#define SIB_H
+
+#include <stdexcept>
+// #include <cstring>
+#include "Storage.hpp"
+#include "Utils.hpp"
+
+class SIB
+{
+
+public:
+
+    static int32_t getScaledIndex(const uint8_t *sequence, int *sequence_current_index, uint8_t modrm_mod_value,
+                                  Storage *storage)
+    {
+        uint8_t byte_value = sequence[(*sequence_current_index)++];
+
+        uint8_t scale = (byte_value & ((uint8_t)0b11000000)) >> 6;
+        uint8_t index = (byte_value & ((uint8_t)0b00111000)) >> 3;
+        uint8_t base = byte_value & ((uint8_t)0b00000111);
+
+        if (index == 0b100)
+        {
+            throw std::logic_error("index=100 not supported in SIB");
+        }
+
+        uint8_t multiplier = ((uint8_t)1) << scale;
+
+        struct StorageArgs index_storage_args;
+        index_storage_args.storage_type = R32;
+        index_storage_args.address = index;
+
+        int32_t scaled_index = storage->load<int32_t>(index_storage_args) * ((int32_t)multiplier);
+        if (base == 0b101 && modrm_mod_value == 0b00)
+        {   
+            /*
+            no base in this case
+            refer Table 2-3. 32-Bit Addressing Forms with the SIB Byte 
+            */
+            int32_t displacement;
+            std::memcpy(&displacement, &(sequence[*sequence_current_index]), sizeof(int32_t));
+            (*sequence_current_index) += sizeof(int32_t);
+            return scaled_index + displacement;
+        }
+
+        struct StorageArgs base_storage_args;
+        base_storage_args.storage_type = R32;
+        base_storage_args.address = base;
+
+        int32_t base_value = storage->load<int32_t>(base_storage_args);
+
+        return base_value + scaled_index;
+    }
+};
+
+#endif
