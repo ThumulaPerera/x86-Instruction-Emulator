d05c0d1bb03b4c7eeceab2a61d690cd40322fa47
Thumula Perera <thumulaperera@gmail.com>
2021-10-24 11:26:27 +0530

    Add helper methods to detect OF,CF for 32b Add Sub

--- emulator/FlagHandler.hpp
+++ emulator/FlagHandler.hpp
@@ -4,14 +4,15 @@
 #include "Storage.hpp"
 
 #include <string>
+#include <math.h>
 
 class FlagHandler
 {
-    private:
-        // Disallow creating an instance of this object externally
-        FlagHandler();   
+private:
+    // Disallow creating an instance of this object externally
+    FlagHandler();
 
-        // methods to handle individual flags
+    // methods to handle individual flags
 
     static void handleCF(bool carry, Storage *storage)
     {
@@ -28,12 +29,12 @@ class FlagHandler
     template <class T>
     static void handlePF(T result, Storage *storage)
     {
-        uint8_t lsbyte = result & 0xff;   // extract the least significant byte of the result
+        uint8_t lsbyte = result & 0xff; // extract the least significant byte of the result
         bool parity = 1;
         while (lsbyte)
         {
             parity = !parity;
-            lsbyte = lsbyte & (lsbyte - 1);  // Unset rightmost set bit
+            lsbyte = lsbyte & (lsbyte - 1); // Unset rightmost set bit
         }
 
         if (parity)
@@ -63,10 +64,12 @@ class FlagHandler
     static void handleSF(T result, Storage *storage)
     {
         T msb = (result >> ((sizeof(T) * 8) - 1));
-        if (msb) {
+        if (msb)
+        {
             storage->setFlag(SF);
         }
-        else{
+        else
+        {
             storage->resetFlag(SF);
         }
     };
@@ -83,40 +86,88 @@ class FlagHandler
         }
     };
 
-    public:
-        template <class T>
-        static void setFlags(T result,
-            Storage *storage, 
-            enum FlagType flagsAffected[], 
-            int flagsAffectedCount, 
-            bool carry = false, 
-            bool oveflow = false)
+public:
+    template <class T>
+    static void setFlags(T result,
+                         Storage *storage,
+                         enum FlagType flagsAffected[],
+                         int flagsAffectedCount,
+                         bool carry = false,
+                         bool oveflow = false)
+    {
+        for (int i = 0; i < flagsAffectedCount; i++)
         {
-            for (int i = 0; i < flagsAffectedCount; i++)
+            std::cout << "flag: " << flagsAffected[i] << std::endl;
+
+            FlagType flagType = flagsAffected[i];
+
+            switch (flagType)
             {
-                std::cout << "flag: " << flagsAffected[i] << std::endl;
+            case CF:
+                handleCF(carry, storage);
+                break;
 
-                FlagType flagType = flagsAffected[i];
+            case ZF:
+                handleZF(result, storage);
+                break;
 
-                switch (flagType)
-                {
-                case CF:
-                    handleCF(carry, storage);
-                    break;
-
-                case ZF:
-                    handleZF(result, storage);
-                    break;
-
-                default:
-                    printf("Unknown Flag Type %x\n", flagType);
-                    throw std::logic_error("Flag not implemented");
-                    break;
-                }
+            default:
+                printf("Unknown Flag Type %x\n", flagType);
+                throw std::logic_error("Flag not implemented");
+                break;
             }
         }
+    }
 
-        ~FlagHandler(){};
+    template <class T>
+    static bool is32BitAddOveflow(T op1, T op2)
+    {
+        // overflow occurs if and only if the result has the opposite sign
+
+        int32_t signed_op1 = (int32_t)op1;
+        int32_t signed_op2 = (int32_t)op2;
+        int32_t result = op1 + op2;
+        if (op1 > 0 && op2 > 0 && result < 0)
+            return true;
+        if (op1 < 0 && op2 < 0 && result > 0)
+            return true;
+        return false;
+    }
+
+    template <class T>
+    static bool is32BitSubOveflow(T op1, T op2)
+    {
+        // overflow occurs if and only if the result has the same sign as the subtrahend
+
+        int32_t signed_op1 = (int32_t)op1;
+        int32_t signed_op2 = (int32_t)op2;
+        int32_t result = op1 - op2;
+        if (signbit(result) ^ signbit(op2))
+            return false;
+        return true;
+    }
+
+    template <class T>
+    static bool is32BitAddCarry(T op1, T op2)
+    {
+        uint32_t unsigned_op1 = (int32_t)op1;
+        uint32_t unsigned_op2 = (int32_t)op2;
+        if (unsigned_op1 > UINT32_MAX - unsigned_op2)
+            return true;
+        return false;
+    }
+
+    template <class T>
+    static bool is32BitSubCarry(T op1, T op2)
+    {
+        uint32_t unsigned_op1 = (int32_t)op1;
+        uint32_t unsigned_op2 = (int32_t)op2;
+        if (unsigned_op1 < unsigned_op2)
+            return true;
+        return false;
+    }
+
+    ~FlagHandler(){};
 };
 
 #endif
