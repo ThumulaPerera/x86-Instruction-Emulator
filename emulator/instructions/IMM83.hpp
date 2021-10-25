19d294c6324c5896090635f49bce4041bd11cc47
Thumula Perera <thumulaperera@gmail.com>
2021-10-25 11:44:16 +0530

    Add flags for IMM83

--- emulator/instructions/IMM83.hpp
+++ emulator/instructions/IMM83.hpp
@@ -5,6 +5,7 @@
 
 #include "AbstractInstruction.hpp"
 #include "../ModRM.hpp"
+#include "../FlagHandler.hpp"
 
 class IMM83 : public AbstractInstruction
 {
@@ -24,33 +25,54 @@ public:
         int32_t imm = (int32_t)(this->getImmediateValue<int8_t>());
         int32_t regMemoryOperand = this->storage->load<int32_t>(storageArgs);
         int32_t result;
+        std::vector<FlagType> flagsAffected;
+        bool hasOverflow = false;
+        bool hasCarry = false;
 
         switch (opcodeExtension)
         {
         case 0:
+        {
             result = regMemoryOperand + imm;
+            hasCarry = FlagHandler::is32BitAddCarry(regMemoryOperand, imm);
+            hasOverflow = FlagHandler::is32BitAddOveflow(regMemoryOperand, imm);
+            flagsAffected.insert(flagsAffected.end(), {OF, SF, ZF, PF, CF});
             break;
-
+        }
         case 1:
+        {
             result = regMemoryOperand | imm;
+            flagsAffected.insert(flagsAffected.end(), {OF, SF, ZF, PF, CF});
             break;
-
+        }
         case 4:
+        {
             result = regMemoryOperand & imm;
+            flagsAffected.insert(flagsAffected.end(), {OF, SF, ZF, PF, CF});
             break;
-
+        }
         case 5:
+        {
             result = regMemoryOperand - imm;
+            hasCarry = FlagHandler::is32BitAddCarry(regMemoryOperand, imm);
+            hasOverflow = FlagHandler::is32BitAddOveflow(regMemoryOperand, imm);
+            flagsAffected.insert(flagsAffected.end(), {OF, SF, ZF, PF, CF});
             break;
-
+        }
         case 6:
+        {
             result = regMemoryOperand ^ imm;
+            flagsAffected.insert(flagsAffected.end(), {OF, SF, ZF, PF, CF});
             break;
-
+        }
         default:
+        {
             throw std::logic_error("Opcode extension not implemented for opcode 83");
             break;
         }
+        }
+        FlagHandler::setFlags(result, this->storage, flagsAffected, hasCarry, hasOverflow);
+
         std::cout << "result = " << result << std::endl;
         this->storage->save<int32_t>(result, storageArgs);
         free(modRMByte);
